c9844c9d6d0cb80cc678821418305a43ea0f19f8
李晨扬 <chenyang.li@melot.cn>
2018-04-16 18:33:17 +0800

    增加分页参数，用于前后台传参

--- src/main/java/com/Gaokao/controller/CollegeController.java
+++ src/main/java/com/Gaokao/controller/CollegeController.java
@@ -1,10 +1,12 @@
 package com.Gaokao.controller;
 
+import com.Gaokao.entity.Page;
 import com.Gaokao.service.CollegeInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -18,9 +20,10 @@ public class CollegeController {
 
     @RequestMapping("/selectAllCollege")
     @ResponseBody
-    public List selectAllCollege(Model model){
+    public Page selectAllCollege(@RequestParam(value="pageNum", defaultValue="1")int pageNum, Model model){
         List collegeList = collegeInfoService.getAllCollege();
-       // model.addAttribute("collegeList",collegeList);
-        return collegeList;
+        Page pg = new Page();
+        pg.setRow(collegeList,pageNum);
+        return pg;
     }
 }
